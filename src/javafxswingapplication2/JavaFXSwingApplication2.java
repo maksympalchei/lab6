ea5727b204ee48c0106ea3812836f00ce94acd07
Maxym Palchei <maksimpalchey@gmail.com>
2024-04-11 22:28:19 +0300

    first commit

---
+++ src/javafxswingapplication2/JavaFXSwingApplication2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXSwingMain.java to edit this template
+ */
+package javafxswingapplication2;
+
+import javax.swing.JFrame;
+/**
+ *
+ * @author korjk
+ */
+public class JavaFXSwingApplication2 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewJFrame frame = new NewJFrame();
+        frame.setVisible(true);
+    }
+}
